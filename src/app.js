bf53731852ae18ef507db2c4c55f185ab5c28d89
unknown <anubhav.singh0816@gmail.com>
2020-03-27 20:00:10 +0530

    setup for heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const forecast = require('./utils/forecast.js')
 // console.log(path.join(__dirname,'../public'))
 
 const app = express()
+const port = process.env.PORT||3000
 
 //Define path for express config
 
@@ -91,6 +92,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("server is set")
 })
